7246a5447c36ac037142b16c01307a4275002ede
hehongyu666 <934245085@qq.com>
2025-03-19 19:39:07 +0800

    保留ema 去除ddp 多卡训练

--- main.py
+++ main.py
@@ -1,16 +1,260 @@
-# 这是一个示例 Python 脚本。
+# ------------------------------------------------------------------------
+# LW-DETR
+# Copyright (c) 2024 Baidu. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from Conditional DETR (https://github.com/Atten4Vis/ConditionalDETR)
+# Copyright (c) 2021 Microsoft. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Modified from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# ------------------------------------------------------------------------
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+import argparse
+import datetime
+import json
+import random
+import time
+from pathlib import Path
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+from datasets import build_dataset, get_coco_api_from_dataset
+from engine import evaluate, train_one_epoch
+from models import build_model
+from util.drop_scheduler import drop_scheduler
+from util.get_param_dicts import get_param_dict
+import util.misc as utils
+from util.utils import ModelEma, BestMetricHolder, clean_state_dict
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
 
-# 按间距中的绿色按钮以运行脚本。
+def main(args):
+
+    print(args)
+
+    device = torch.device(args.device)
+
+    # fix the seed for reproducibility
+    seed = args.seed
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    model, criterion, postprocessors = build_model(args)
+    model.to(device)
+    
+    ema_m = ModelEma(model, decay=args.ema_decay)  # 保留全部ema
+
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('number of params:', n_parameters)
+
+    param_dicts = get_param_dict(args, model)
+
+    optimizer = torch.optim.AdamW(param_dicts, lr=args.lr, 
+                                  weight_decay=args.weight_decay)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, args.lr_drop)
+
+    dataset_train = build_dataset(image_set='train', args=args)
+    dataset_val = build_dataset(image_set='val', args=args)
+
+    sampler_train = torch.utils.data.RandomSampler(dataset_train)
+    sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+
+    batch_sampler_train = torch.utils.data.BatchSampler(
+        sampler_train, args.batch_size, drop_last=True)
+
+    data_loader_train = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
+                                   collate_fn=utils.collate_fn, num_workers=args.num_workers)
+    data_loader_val = DataLoader(dataset_val, args.batch_size, sampler=sampler_val,
+                                 drop_last=False, collate_fn=utils.collate_fn, 
+                                 num_workers=args.num_workers)
+
+    base_ds = get_coco_api_from_dataset(dataset_val)
+
+    if args.pretrain_weights is not None:
+        checkpoint = torch.load(args.pretrain_weights, map_location='cpu')
+        # add support to exclude_keys
+        # e.g., when load object365 pretrain, do not load `class_embed.[weight, bias]`
+        if args.pretrain_exclude_keys is not None:
+            assert isinstance(args.pretrain_exclude_keys, list)
+            for exclude_key in args.pretrain_exclude_keys:
+                checkpoint['model'].pop(exclude_key)
+        if args.pretrain_keys_modify_to_load is not None:
+            from util.obj365_to_coco_model import get_coco_pretrain_from_obj365
+            assert isinstance(args.pretrain_keys_modify_to_load, list)
+            for modify_key_to_load in args.pretrain_keys_modify_to_load:
+                checkpoint['model'][modify_key_to_load] = get_coco_pretrain_from_obj365(
+                    model.state_dict()[modify_key_to_load],
+                    checkpoint['model'][modify_key_to_load]
+                )
+        model.load_state_dict(checkpoint['model'], strict=False)
+        
+        del ema_m
+        ema_m = ModelEma(model)
+
+    output_dir = Path(args.output_dir)
+    
+    if args.resume:
+        checkpoint = torch.load(args.resume, map_location='cpu')
+        model.load_state_dict(checkpoint['model'], strict=True)
+        
+
+        if 'ema_model' in checkpoint:
+            ema_m.module.load_state_dict(clean_state_dict(checkpoint['ema_model']))
+        else:
+            del ema_m
+            ema_m = ModelEma(model) 
+
+        if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
+            checkpoint['optimizer']["param_groups"] = optimizer.state_dict()["param_groups"]
+            checkpoint['lr_scheduler'].pop("step_size")
+            checkpoint['lr_scheduler'].pop("_last_lr")
+            
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+            args.start_epoch = checkpoint['epoch'] + 1
+
+    if args.eval:
+        test_stats, coco_evaluator = evaluate(
+            model, criterion, postprocessors, data_loader_val, base_ds, device, args)
+        if args.output_dir:
+            utils.save_on_master(coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth")
+        return
+    
+    # for drop
+    total_batch_size = args.batch_size
+    num_training_steps_per_epoch = (len(dataset_train) + total_batch_size - 1) // total_batch_size
+    schedules = {}
+    if args.dropout > 0:
+        schedules['do'] = drop_scheduler(
+            args.dropout, args.epochs, num_training_steps_per_epoch,
+            args.cutoff_epoch, args.drop_mode, args.drop_schedule)
+        print("Min DO = %.7f, Max DO = %.7f" % (min(schedules['do']), max(schedules['do'])))
+
+    if args.drop_path > 0:
+        schedules['dp'] = drop_scheduler(
+            args.drop_path, args.epochs, num_training_steps_per_epoch,
+            args.cutoff_epoch, args.drop_mode, args.drop_schedule)
+        print("Min DP = %.7f, Max DP = %.7f" % (min(schedules['dp']), max(schedules['dp'])))
+
+    print("Start training")
+    start_time = time.time()
+    best_map_holder = BestMetricHolder(use_ema=args.use_ema)
+    for epoch in range(args.start_epoch, args.epochs):
+        epoch_start_time = time.time()
+
+        train_stats = train_one_epoch(
+            model, criterion, data_loader_train, optimizer, device, epoch,
+            args.clip_max_norm, ema_m=ema_m, schedules=schedules, 
+            num_training_steps_per_epoch=num_training_steps_per_epoch,
+            vit_encoder_num_layers=args.vit_encoder_num_layers, args=args)
+        train_epoch_time = time.time() - epoch_start_time
+        train_epoch_time_str = str(datetime.timedelta(seconds=int(train_epoch_time)))
+        
+        lr_scheduler.step()
+        if args.output_dir:
+            checkpoint_paths = [output_dir / 'checkpoint.pth']
+            # extra checkpoint before LR drop and every `checkpoint_interval` epochs
+            if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % args.checkpoint_interval == 0:
+                checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
+            for checkpoint_path in checkpoint_paths:
+                weights = {
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'lr_scheduler': lr_scheduler.state_dict(),
+                    'epoch': epoch,
+                    'args': args,
+                }
+                if args.use_ema:
+                    weights.update({
+                        'ema_model': ema_m.module.state_dict(),
+                    })
+                utils.save_on_master(weights, checkpoint_path)
+
+        test_stats, coco_evaluator = evaluate(
+            model, criterion, postprocessors, data_loader_val, base_ds, device, args=args
+        )
+        
+        map_regular = test_stats['coco_eval_bbox'][0]
+        _isbest = best_map_holder.update(map_regular, epoch, is_ema=False)
+        if _isbest:
+            checkpoint_path = output_dir / 'checkpoint_best_regular.pth'
+            utils.save_on_master({
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'lr_scheduler': lr_scheduler.state_dict(),
+                'epoch': epoch,
+                'args': args,
+            }, checkpoint_path)
+        log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
+                     **{f'test_{k}': v for k, v in test_stats.items()},
+                     'epoch': epoch,
+                     'n_parameters': n_parameters}
+
+        log_stats.update(best_map_holder.summary())
+        
+        if args.use_ema:
+            ema_test_stats, _ = evaluate(
+                ema_m.module, criterion, postprocessors, data_loader_val, base_ds, device, args=args
+            )
+            log_stats.update({f'ema_test_{k}': v for k,v in ema_test_stats.items()})
+            map_ema = ema_test_stats['coco_eval_bbox'][0]
+            _isbest = best_map_holder.update(map_ema, epoch, is_ema=True)
+            if _isbest:
+                checkpoint_path = output_dir / 'checkpoint_best_ema.pth'
+                utils.save_on_master({
+                    'model': ema_m.module.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'lr_scheduler': lr_scheduler.state_dict(),
+                    'epoch': epoch,
+                    'args': args,
+                }, checkpoint_path)
+
+        # epoch parameters
+        ep_paras = {
+                'epoch': epoch,
+                'n_parameters': n_parameters
+            }
+        log_stats.update(ep_paras)
+        try:
+            log_stats.update({'now_time': str(datetime.datetime.now())})
+        except:
+            pass
+        log_stats['train_epoch_time'] = train_epoch_time_str
+        epoch_time = time.time() - epoch_start_time
+        epoch_time_str = str(datetime.timedelta(seconds=int(epoch_time)))
+        log_stats['epoch_time'] = epoch_time_str
+        
+        if args.output_dir and utils.is_main_process():
+            with (output_dir / "log.txt").open("a") as f:
+                f.write(json.dumps(log_stats) + "\n")
+
+            # for evaluation logs
+            if coco_evaluator is not None:
+                (output_dir / 'eval').mkdir(exist_ok=True)
+                if "bbox" in coco_evaluator.coco_eval:
+                    filenames = ['latest.pth']
+                    if epoch % 50 == 0:
+                        filenames.append(f'{epoch:03}.pth')
+                    for name in filenames:
+                        torch.save(coco_evaluator.coco_eval["bbox"].eval,
+                                   output_dir / "eval" / name)
+
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Training time {}'.format(total_time_str))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    parser = argparse.ArgumentParser('LWDETR training and evaluation script', parents=[get_args_parser()])
+    args = parser.parse_args()
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    if args.output_dir:
+        Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+
+    main(args)
